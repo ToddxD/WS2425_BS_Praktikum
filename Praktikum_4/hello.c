8a1f8046467569525fabd8c8691f3342b1eeff73
Hendrik <51798868+ToddxD@users.noreply.github.com>
2025-01-08 21:06:55 +0100

    prakt 4

--- Praktikum_4/hello.c
+++ Praktikum_4/hello.c
@@ -1,11 +1,15 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
+static int translate_shift = 3;
+module_param(translate_shift, int, S_IRUGO);
+
 static int hello_init(void)
 {
-  printk(KERN_INFO "Hello, world\n");
+  printk(KERN_INFO "Die Zahlt lautet: %d\n", translate_shift);
   return 0;
 }
 
