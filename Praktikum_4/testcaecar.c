448edc2771ba6ba1dbcf85af21a6cc828e0b8c80
Hendrik <51798868+ToddxD@users.noreply.github.com>
2025-01-14 13:27:58 +0100

    prakt 4

--- Praktikum_4/testcaecar.c
+++ Praktikum_4/testcaecar.c
@@ -7,7 +7,7 @@
 int main() {
   printf("before...");
   printf("%s\n", strerror(errno));
-  int fd = open("/dev/encrypt", 0_RDWR);
+  int fd = open("/dev/encrypt", O_RDWR);
 
   printf("open...");
   printf("%s\n", strerror(errno));
