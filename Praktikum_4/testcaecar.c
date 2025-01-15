ba51e240976c79c6ab7dc7a7afd8b9581ffeca2f
Hendrik <51798868+ToddxD@users.noreply.github.com>
2025-01-15 08:44:07 +0100

    prakt 4

--- Praktikum_4/testcaecar.c
+++ Praktikum_4/testcaecar.c
@@ -7,25 +7,32 @@
 
 int main() {
   int fd = open("/dev/decrypt", O_RDWR);
-  printf("%s\n", strerror(errno));
-  int fd2 = open("/dev/decrypt", O_RDWR);
-  //int fd = open("/dev/encrypt", O_RDWR);
+  int fd2 = open("/dev/encrypt", O_RDWR);
 
   printf("open...");
   printf("%s\n", strerror(errno));
 
   char* buf = "CBA//cDb a";
+  char* buf2 = "zyx// AZXY";
   write(fd, buf, 10);
+  write(fd2, buf2, 10);
 
   printf("write...");
   printf("%s\n", strerror(errno));
 
   char* out = malloc(sizeof(char) * 10);
+  char* out2 = malloc(sizeof(char) * 10);
   read(fd, out, 10);
-  printf("expected: zyx// AZXY\n");
+  read(fd2, out2, 10);
+  printf("expectedDec: zyx// AZXY\n");
   printf("verschluesselt: %s\n", out);
+  printf("expectedEnc: CBA//cDb a\n");
+  printf("entschluesselt: %s\n", out2);
   printf("%s\n", strerror(errno));
 
   close(fd);
+  close(fd2);
+  free(out);
+  free(out2);
   return 0;
 }
